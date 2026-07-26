d772a30c42cd0bb28d9ebfdaecd07b56e2af1175
deltakiro <deltakilone@hotmail.com>
2026-07-26 16:09:04 +0530

    ch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import { unified } from "@astrojs/markdown-remark"; // 1. Added unified import
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astronaut.github.io",
+  site: "https://starrocketry.github.io", // Updated site URL
   markdown: {
-    // 2. Wrapped plugins inside processor: unified(...)
-    processor: unified({
-      remarkPlugins: [remarkMath],
-      rehypePlugins: [rehypeKatex],
-    }),
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
-  // If you are using the @astrojs/mdx integration, add them there too:
   integrations: [],
 });
